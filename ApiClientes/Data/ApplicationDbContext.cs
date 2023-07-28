af1378b5088ae62932a613c7651599625218d9b4
jos-gabriel <jos.gabrie@gmail.com>
2023-07-28 04:32:59 -0600

    feat(API): se agrega la primer base de datos

---
+++ ApiClientes/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APIClientes.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIClientes.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ ApiClientes/Modelos/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIClientes.Modelos
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Nombres { get; set; }
+        [Required]
+        public String Apellidos { get; set; }
+        [Required]
+        public String Direccion { get; set; }
+        [Required]
+        public String Telefono { get; set; }
+
+    }
+}
