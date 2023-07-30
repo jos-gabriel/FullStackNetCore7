3999cd536eedb308b3d3f222432dd9d40ca569a5
jos-gabriel <jos.gabrie@gmail.com>
2023-07-29 18:15:12 -0600

    feat(api): register y tabla user
    Se agrega método para registrar usuario DTO y tabla user

--- ApiClientes/Migrations/20230729215523_agregarUsuarioDb.Designer.cs
+++ ApiClientes/Migrations/20230729215523_agregarUsuarioDb.Designer.cs
@@ -3,6 +3,7 @@ using APIClientes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APIClientes.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230729215523_agregarUsuarioDb")]
+    partial class agregarUsuarioDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ApiClientes/Repositorio/UserRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using APIClientes.Data;
+using APIClientes.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIClientes.Repositorio
+{
+    public class UserRepositorio : IUserRepositorio
+    {
+
+        private readonly ApplicationDbContext _db;
+        public UserRepositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+        public Task<string> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> Register(User user, string password)
+        {
+            try
+            {
+                if (await UserExiste(user.UserName))
+                {
+                    return -1;
+                }
+                CrearPasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+                user.PasswordHash = passwordHash;
+                user.PasswoedSalt = passwordSalt;
+
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
+                return user.Id;
+            }
+            catch (Exception)
+            {
+
+                return -500;
+            }
+        }
+
+        public async Task<bool> UserExiste(string userName)
+        {
+            if (await _db.Users.AnyAsync(x => x.UserName.ToLower().Equals(userName.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CrearPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
