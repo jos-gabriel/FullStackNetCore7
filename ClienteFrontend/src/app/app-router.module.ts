57bcb7dce1ea81652c3eb6cc3867b16b8d69c3f6
jos-gabriel <jos.gabrie@gmail.com>
2023-08-02 22:03:49 -0600

    feat(delete-cliente): Borrar Datos - Pagina de Confirmación Boótn para confirmar el borrado y ruteo a la página

--- ClienteFrontend/src/app/app-router.module.ts
+++ ClienteFrontend/src/app/app-router.module.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { ClientesComponent } from "./clientes/clientes.component";
 import { NgModule } from "@angular/core";
 import { CrearClienteComponent } from "./crear-cliente/crear-cliente.component";
+import { DeleteClienteComponent } from "./delete-cliente/delete-cliente.component";
 
 // route
 const routes:Routes = [
     {path: '', component: ClientesComponent},
     {path: 'clientes', component: ClientesComponent},
-    {path: 'crear-cliente', component: CrearClienteComponent}
+    {path: 'crear-cliente', component: CrearClienteComponent},
+    {path: 'delete-cliente/:id',component: DeleteClienteComponent}
 ]
 
 @NgModule({
