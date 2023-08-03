c5d96cf9decda7a44a86cf0dfa34b703e1974ae0
jos-gabriel <jos.gabrie@gmail.com>
2023-08-03 02:03:50 -0600

    feat(register): formulario register estilo para el componente boton para el header cambio de tema design

--- ClienteFrontend/src/app/app-router.module.ts
+++ ClienteFrontend/src/app/app-router.module.ts
@@ -3,13 +3,15 @@ import { ClientesComponent } from "./clientes/clientes.component";
 import { NgModule } from "@angular/core";
 import { CrearClienteComponent } from "./crear-cliente/crear-cliente.component";
 import { DeleteClienteComponent } from "./delete-cliente/delete-cliente.component";
+import { RegisterComponent } from "./register/register.component";
 
 // route
 const routes:Routes = [
     {path: '', component: ClientesComponent},
     {path: 'clientes', component: ClientesComponent},
     {path: 'crear-cliente', component: CrearClienteComponent},
-    {path: 'delete-cliente/:id',component: DeleteClienteComponent}
+    {path: 'delete-cliente/:id',component: DeleteClienteComponent},
+    {path: 'register', component: RegisterComponent}
 ]
 
 @NgModule({
--- ClienteFrontend/src/app/register/register.component.ts
+++ ClienteFrontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ])]
+    })
+  }
 
 }
